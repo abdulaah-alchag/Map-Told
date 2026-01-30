893ceca65ddf4be6c74856f1535bae8d4aa763d1
Dandy Nagel <github@dandynagel.com>
2026-01-30 14:21:05 +0100

    added location form layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { GoLocation } from 'react-icons/go';
+import { GoLocation, GoSearch } from 'react-icons/go';
 
 import { App_Logo } from '@/components/elements';
 
@@ -7,10 +7,11 @@ export const Home = () => {
     <>
       <title>MapTold - Home</title>
       <main className=''>
+        {/* HERO SECTION ============== */}
         <div id='Hero' className='hero bg-base-200 hero-bg-image min-h-screen'>
-          <div className='hero-content h-full pt-10 text-center'>
+          <div className='hero-content h-full pt-10 pb-20 text-center'>
             <div className='flex h-[80%] max-w-md flex-col items-center justify-between'>
-              <App_Logo className='w-30'></App_Logo>
+              <App_Logo className='w-[40%]'></App_Logo>
               <h1 className='px-6 text-5xl font-bold'>Dein virtueller Aussichtsturm</h1>
               <p className='px-10 py-8 italic'>
                 Gib einen geografischen Punkt an und erhalte relevante Informationen zu seiner
@@ -22,6 +23,27 @@ export const Home = () => {
               </button>
             </div>
           </div>
+        </div>
+
+        {/* FORM SECTION ============== */}
+        <div id='Location-Form' className='bg-mt-color-1 flex flex-col justify-center p-10'>
+          <h2>Zielort eingeben:</h2>
+          <form className='location-form-grid'>
+            <input type='text' className='input' placeholder='Strasse' />
+            <div className='grid grid-cols-[30%_1fr] gap-2'>
+              <input type='text' className='input' placeholder='PLZ' />
+              <input type='text' className='input' placeholder='Stadt' />
+            </div>
+            <div className='grid grid-cols-2 gap-2 pt-2'>
+              <input type='number' className='input' placeholder='Longitude' />
+              <input type='number' className='input' placeholder='Latitude' />
+            </div>
+
+            <button className='btn btn-secondary m-auto mt-4 w-30' type='button'>
+              <GoSearch />
+              Suchen
+            </button>
+          </form>
         </div>
       </main>
     </>
