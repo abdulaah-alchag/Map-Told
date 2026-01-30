29ead9c54c70f79b44d48e8427c75778a9328fba
Dandy Nagel <94107254+GittyX3b@users.noreply.github.com>
2026-01-30 13:02:47 +0100

    mobile layout hero section (#16)
    * mobile hero finished
    
    * changes navbar and main layout
    
    * changes css and fonts

--- frontend/src/components/elements/Logo.tsx
+++ frontend/src/components/elements/Logo.tsx
@@ -1,10 +1,22 @@
 import maptoldIcon from '@assets/images/maptold-icon.png';
-import maptoldLogo from '@assets/images/maptold-icon.png';
+import maptoldLogo from '@assets/images/maptold-logo.png';
 
 export const App_Icon = ({ className = '' }: { className?: string }) => {
-  return <img src={maptoldIcon} alt='MapTold Icon' className={`h-full ${className}`} />;
+  return (
+    <img
+      src={maptoldIcon}
+      alt='MapTold Icon'
+      className={`pointer-events-none h-full select-none ${className}`}
+    />
+  );
 };
 
-export const App_Logo = () => {
-  return <img src={maptoldLogo} alt='MapTold Logo' />;
+export const App_Logo = ({ className = '' }: { className?: string }) => {
+  return (
+    <img
+      src={maptoldLogo}
+      alt='MapTold Logo'
+      className={`pointer-events-none select-none ${className}`}
+    />
+  );
 };
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,29 @@
+import { GoLocation } from 'react-icons/go';
+
+import { App_Logo } from '@/components/elements';
+
 export const Home = () => {
   return (
     <>
-      <title>MapTold - Home.tsx</title>
-      <h2>this is the Home.tsx...</h2>
+      <title>MapTold - Home</title>
+      <main className=''>
+        <div id='Hero' className='hero bg-base-200 hero-bg-image min-h-screen'>
+          <div className='hero-content h-full pt-10 text-center'>
+            <div className='flex h-[80%] max-w-md flex-col items-center justify-between'>
+              <App_Logo className='w-30'></App_Logo>
+              <h1 className='px-6 text-5xl font-bold'>Dein virtueller Aussichtsturm</h1>
+              <p className='px-10 py-8 italic'>
+                Gib einen geografischen Punkt an und erhalte relevante Informationen zu seiner
+                Umgebung, serviert von unserer Standort-KI.
+              </p>
+              <button className='btn btn-primary'>
+                <GoLocation />
+                Aktuellen Standort zeigen
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 };
