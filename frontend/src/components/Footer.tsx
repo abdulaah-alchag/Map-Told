2a2fc141c4e26a25b9c9f27bf298be99e6ede0bd
Dandy Nagel <github@dandynagel.com>
2026-02-02 19:10:47 +0100

    added narratives to landing page

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = ({ className = '' }: { className?: string }) => {
   return (
-    <footer className={`bg-mt-color-1 flex items-center justify-center pt-50 pb-20 ${className}`}>
+    <footer className={`bg-mt-color-1 flex items-center justify-center pt-20 pb-15 ${className}`}>
       <span className='text-xs'>©MapTold 2026</span>
     </footer>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -18,20 +18,20 @@ export const Home = () => {
                 Gib einen geografischen Punkt an und erhalte relevante Informationen zu seiner
                 Umgebung, serviert von unserer Standort-KI.
               </p>
-              <button className='btn btn-primary w-70 lg:w-fit'>
+              <a href='#Location-Form' className='btn btn-primary w-70 lg:w-fit'>
                 <GoLocation />
-                Aktuellen Standort zeigen
-              </button>
+                Zielort eingeben
+              </a>
             </div>
           </div>
         </div>
 
-        {/* FORM SECTION ============== */}
+        {/* FORM SECTION =============== */}
         <div id='Location-Form' className='bg-mt-color-4 flex flex-col justify-center lg:flex-row'>
-          <div className='bg-mt-color-5 hidden h-150 w-[50%] lg:flex'>
+          <div className='bg-mt-color-5 hidden h-150 w-[50%] xl:flex'>
             <div className='form-beside-image h-full w-full'></div>
           </div>
-          <form className='location-form-grid p-5 pt-20 pb-40 lg:p-20'>
+          <form className='location-form-grid p-5 pt-10 pb-25 lg:px-10'>
             <h2>Zielort eingeben:</h2>
             <input type='text' className='input' placeholder='Strasse' />
             <div className='grid grid-cols-[30%_1fr] gap-2'>
@@ -43,65 +43,101 @@ export const Home = () => {
               <input type='number' className='input' placeholder='Latitude' />
             </div>
 
-            <button className='btn btn-secondary m-auto mt-14 w-70 lg:w-70' type='button'>
+            <button className='btn btn-secondary m-auto mt-14 w-full' type='button'>
               <GoSearch />
               Suchen
             </button>
           </form>
         </div>
-
-        {/* MAP SECTION ============== */}
-        <div id='Map-Section' className='bg-mt-color-1 h-full'>
-          <div className='px-5 pt-15 pb-5 lg:px-20'>
-            <h2>Ergebnisse:</h2>
+        {/* NARRATIVE SECTION ========== */}
+        <div id='Narratives' className='h-200 md:h-280 lg:h-350'>
+          <div id='Narrative-1' className='grid h-1/3 grid-cols-2'>
+            <div className='bg-mt-color-20 flex items-center justify-center p-5 text-center'>
+              <h3>
+                Finde kurzfristig<br></br> interresante Orte
+              </h3>
+            </div>
+            <div className='bg-mt-color-5'>
+              <div className='narrative-image-1 h-full'></div>
+            </div>
           </div>
-          <div id='Leaflet-Container' className='h-full'>
-            <LeafletMap className='h-200'></LeafletMap>
+          <div id='Narrative-2' className='grid h-1/3 grid-cols-2'>
+            <div className='bg-mt-color-5'>
+              <div className='narrative-image-2 h-full w-full'></div>
+            </div>
+            <div className='bg-mt-color-13 flex items-center justify-center p-5 text-center'>
+              <h3>
+                Plane gemeinsame <br></br> Abenteuer
+              </h3>
+            </div>
+          </div>
+          <div id='Narrative-3' className='grid h-1/3 grid-cols-2'>
+            <div className='bg-mt-color-30 flex items-center justify-center p-5 text-center'>
+              <h3>
+                Checke eine neue<br></br> Wohngegend
+              </h3>
+            </div>
+            <div className='bg-mt-color-5'>
+              <div className='narrative-image-3 h-full w-full'></div>
+            </div>
           </div>
         </div>
 
-        {/* RESULTS SECTION ============== */}
-        <div
-          id='Results'
-          className='grid grid-rows-[min-content_min-content_1fr] gap-10 p-5 pb-35 lg:p-20 lg:pb-40'
-        >
-          <div id='Weather' className='grid grid-cols-[33%_1fr] gap-5'>
-            <div>
-              <b>Aktuelles Wetter:</b>
+        {/* RESPONSE SECTION =========== */}
+        <div id='Response' className='hidden'>
+          {/* MAP and FILTER =========== */}
+          <div id='Map-Section' className='bg-mt-color-1 h-full'>
+            <div className='px-5 pt-15 pb-5 lg:px-20'>
+              <h2>Ergebnisse:</h2>
             </div>
-            <div className='flex justify-between'>
-              <span id='Weather-Icon' className='text-2xl text-yellow-500'>
-                <GoSun />
-              </span>
-              <span id='Temperature'>12°C</span>
-              <span id='Wind-Description'>windy</span>
+            <div id='Leaflet-Container' className='h-full'>
+              <LeafletMap className='h-200'></LeafletMap>
             </div>
           </div>
-          <div id='Geo-Info' className='grid grid-cols-[33%_1fr] gap-5'>
-            <div>
-              <b>Geografische Informationen:</b>
+
+          {/* RESULTS ================== */}
+          <div
+            id='Results'
+            className='grid grid-rows-[min-content_min-content_1fr] gap-10 p-5 pb-35 lg:p-20 lg:pb-40'
+          >
+            <div id='Weather' className='grid grid-cols-[33%_1fr] gap-5'>
+              <div>
+                <b>Aktuelles Wetter:</b>
+              </div>
+              <div className='flex justify-between'>
+                <span id='Weather-Icon' className='text-2xl text-yellow-500'>
+                  <GoSun />
+                </span>
+                <span id='Temperature'>12°C</span>
+                <span id='Wind-Description'>windy</span>
+              </div>
             </div>
-            <div className='flex items-center justify-between'>
-              <span id='Geo-Info-1'>84m üNN</span>
-              <span id='Geo-Info-2'>something else</span>
+            <div id='Geo-Info' className='grid grid-cols-[33%_1fr] gap-5'>
+              <div>
+                <b>Geografische Informationen:</b>
+              </div>
+              <div className='flex items-center justify-between'>
+                <span id='Geo-Info-1'>84m üNN</span>
+                <span id='Geo-Info-2'>something else</span>
+              </div>
             </div>
-          </div>
-          <div id='AI-Text' className='grid grid-cols-[1fr] gap-5'>
-            <div className='lg:text-2xl'>
-              <b>Umgebung:</b>
-            </div>
-            <div className='text-justify'>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus aliquid excepturi
-              maxime eligendi ullam repellendus, dolorem explicabo blanditiis laboriosam molestias
-              assumenda alias, illum expedita, quibusdam doloremque consectetur? Accusamus
-              voluptatem quia, laboriosam illum quisquam, commodi voluptate reiciendis, aliquam
-              laudantium officia quas dolores dicta eveniet eius sunt! Nobis delectus ipsa, debitis
-              porro asperiores tenetur perspiciatis architecto nulla ut quaerat ratione quo atque,
-              excepturi fugiat hic unde, dolore ducimus esse quae. Repellendus atque veritatis hic,
-              necessitatibus totam quidem labore provident, animi suscipit quam quis sint sunt
-              reiciendis corporis fugiat facilis tempore earum non ad beatae mollitia rem. Minima
-              molestiae doloribus aliquam provident soluta!
-              <span id='Geo-Info-2'>something else</span>
+            <div id='AI-Text' className='grid grid-cols-[1fr] gap-5'>
+              <div className='lg:text-2xl'>
+                <b>Umgebung:</b>
+              </div>
+              <div className='text-justify'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus aliquid excepturi
+                maxime eligendi ullam repellendus, dolorem explicabo blanditiis laboriosam molestias
+                assumenda alias, illum expedita, quibusdam doloremque consectetur? Accusamus
+                voluptatem quia, laboriosam illum quisquam, commodi voluptate reiciendis, aliquam
+                laudantium officia quas dolores dicta eveniet eius sunt! Nobis delectus ipsa,
+                debitis porro asperiores tenetur perspiciatis architecto nulla ut quaerat ratione
+                quo atque, excepturi fugiat hic unde, dolore ducimus esse quae. Repellendus atque
+                veritatis hic, necessitatibus totam quidem labore provident, animi suscipit quam
+                quis sint sunt reiciendis corporis fugiat facilis tempore earum non ad beatae
+                mollitia rem. Minima molestiae doloribus aliquam provident soluta!
+                <span id='Geo-Info-2'>something else</span>
+              </div>
             </div>
           </div>
         </div>
