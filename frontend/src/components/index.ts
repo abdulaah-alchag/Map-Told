736c63806e8222d1712f2a2883dbcefe6dc05598
Dandy Nagel <github@dandynagel.com>
2026-02-03 15:21:47 +0100

    changed location form to component

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,6 @@
 export { Footer } from './Footer';
 export { Hero } from './Hero.tsx';
+export { LocationForm } from './Location-Form.tsx';
 export { LeafletMap } from './LeafletMap';
 export { Narratives } from './Narratives';
 export { NavBar } from './NavBar';
